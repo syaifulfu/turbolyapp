52dae5b81668c2c4c43e700fab00c3de86a941d7
syaifulfu <msyaifulfuad25@gmail.com>
2022-06-03 19:39:57 +0700

    config environtment prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Custom
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
+  
 end
