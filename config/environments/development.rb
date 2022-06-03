be69686664334ce50b75df156cda9ce11067865c
syaifulfu <msyaifulfuad25@gmail.com>
2022-06-03 19:36:36 +0700

    config db

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Custom
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
+  
 end
